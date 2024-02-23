d8070353628895377316e9f9ebad74f9bc1b21f1
Amelia Miner <ameliamminer@gmail.com>
2024-02-23 00:09:53 -0800

    better debug, cmd line max. dims

--- image_printer_go_brrr.py
+++ image_printer_go_brrr.py
@@ -8,6 +8,7 @@
  * Re-write with a class or two?
  * Would be cool, but maybe scale poorly, to do context-aware unicode blockchars
    ie have a left-half-block if pixel to left is opaque but right isn't
+ * so, so many more good possibilities for command line args...
  * needs some kind of tests... oh boy
 """
 
@@ -18,8 +19,10 @@ from sys import exit
 from os import get_terminal_size
 
 
+# TODO... classes?
 CHARS = ['\u2588', '\u2593', '\u2592', '\u2591', '#', '@', '$', '+', '-', ' ']  #UNICODE_ONLY
 INTERVALS = [255/len(CHARS)*i for i in range(len(CHARS)-1, -1, -1)]
+DEBUG=False
 
 
 def resize_image_to_fit(im, max_height, max_width):
@@ -43,6 +46,8 @@ def resize_image_to_fit(im, max_height, max_width):
         new_height -= 1
         new_width -= 1
 
+    if DEBUG:
+        print(f'Scaled dims h:{im.height}, w:{im.width}')
     return im
 
 def get_char_from_alpha(a):
@@ -60,19 +65,28 @@ def get_char_from_alpha(a):
     return '🚩'  #UNICODE_ONLY
 
 
-def main(img_path, debug=False):
+def main(img_path, dims=None):
     """
     get terminal dimensions,
     generate output,
     print output to terminal
+    :param img_path: str, path to image file
+    :param dims: tuple of ints || None, maximum number of output cells.
+        None defaults to terminal size.
     """
+    if DEBUG:
+        print(f'Begin processing {img_path}')
     # assume a terminal character is about twice as tall as it is wide
-    max_height = get_terminal_size().lines - 1  # -1 accounts for prompt line
-    max_width = get_terminal_size().columns // 2
-    im = Image.open(img_path).convert("RGBA")
-    if debug:
-        max_height = max_height/10 * 9
-        max_width = max_width/10 * 9
+    if dims is not None:
+        max_height = dims[0]
+        max_width = dims[1]
+    else:
+        max_height = get_terminal_size().lines - 1  # -1 accounts for prompt line
+        max_width = get_terminal_size().columns // 2
+    im = Image.open(img_path).convert("RGBA")  # TODO error handling af
+    if DEBUG:
+        print(f'Original dims h:{im.height}, w:{im.width}\n'
+              f'Maximum dims h:{max_height}, w:{max_width}')
     im = resize_image_to_fit(im, max_height, max_width)
 
     output = []
@@ -91,15 +105,33 @@ def main(img_path, debug=False):
     for c in output:
         print(c, end="")
 
+    if DEBUG:
+        print(f'Finished processing {img_path}')
     exit(0)
 
 
 if __name__ == "__main__":
+    """
+    set up and parse args,
+    interpret args,
+    call main
+    """
     argparser = argparse.ArgumentParser()
     argparser.add_argument('-d', '--debug', action='store_true',
             required=False, default=False,
             help='reduce output dims, may print helpful info')
+    # TODO handle bad indiv. values and combinations of these
+    argparser.add_argument('-H', '--max-height', action='store', type=int,
+            required=False, help='WIP; restrict output to this many rows at most')
+    argparser.add_argument('-W', '--max-width', action='store', type=int,
+            required=False, help='WIP; restrict output to this many columns at most')
     argparser.add_argument('imageFilePath')
     args = argparser.parse_args()
-    main(args.imageFilePath, args.debug)
+
+    DEBUG = args.debug
+    dims = None
+    if args.max_height is not None and args.max_width is not None:
+        dims = (args.max_height, args.max_width)
+
+    main(args.imageFilePath, dims)
 
