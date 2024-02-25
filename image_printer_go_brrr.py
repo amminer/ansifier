5d9a9ff6f866850d0a059fd7e34d145435fecbdc
Amelia Miner <ameliamminer@gmail.com>
2024-02-25 10:47:47 -0800

    added char-offset and char-by-brightness settings, allow individual restrictions on either height or width

--- image_printer_go_brrr.py
+++ image_printer_go_brrr.py
@@ -21,6 +21,7 @@ from os import get_terminal_size
 
 # TODO... classes?
 CHARS = ['\u2588', '\u2593', '\u2592', '\u2591', '#', '@', '$', '+', '-', ' ']  #UNICODE_ONLY
+NUM_CHARS = len(CHARS)
 INTERVALS = [255/len(CHARS)*i for i in range(len(CHARS)-1, -1, -1)]
 DEBUG=False
 
@@ -46,13 +47,12 @@ def resize_image_to_fit(im, max_height, max_width):
         new_height -= 1
         new_width -= 1
 
-    if DEBUG:
-        print(f'Scaled dims h:{im.height}, w:{im.width}')
     return im
+
 
 def get_char_from_alpha(a):
     """
-    takes in a transparency value and maps it to a character. The idea
+    Takes in a transparency value and maps it to a character. The idea
     is that the more transparent an image's pixel is, the less "foreground"
     its representative terminal character should have. This is why this
     program is unicode-only... TODO adapt based on runtime environment
@@ -65,7 +65,21 @@ def get_char_from_alpha(a):
     return '🚩'  #UNICODE_ONLY
 
 
-def main(img_path, dims=None):
+def get_char_from_brightness(r, g, b):
+    """
+    Takes in a rgb values and maps their total brightness to a character.
+    Sometimes this is cuter than using alpha (in small outputs for example)
+    :param a: int, min 0, max 255, alpha value of a pixel
+    :return: character fitting alpha value
+    """
+    brightness = r + g + b  # max. 765
+    for i, interval in enumerate(INTERVALS):
+        if brightness >= interval*3:
+            return CHARS[i]
+    return '🚩'  #UNICODE_ONLY
+
+
+def main(img_path, max_height=None, max_width=None, char_by_brightness=False):
     """
     get terminal dimensions,
     generate output,
@@ -77,17 +91,17 @@ def main(img_path, dims=None):
     if DEBUG:
         print(f'Begin processing {img_path}')
     # assume a terminal character is about twice as tall as it is wide
-    if dims is not None:
-        max_height = dims[0]
-        max_width = dims[1]
-    else:
+    if max_height == None:
         max_height = get_terminal_size().lines - 1  # -1 accounts for prompt line
+    if max_width == None:
         max_width = get_terminal_size().columns // 2
     im = Image.open(img_path).convert("RGBA")  # TODO error handling af
     if DEBUG:
         print(f'Original dims h:{im.height}, w:{im.width}\n'
               f'Maximum dims h:{max_height}, w:{max_width}')
     im = resize_image_to_fit(im, max_height, max_width)
+    if DEBUG:
+        print(f'Resized dims h:{im.height}, w:{im.width}')
 
     output = []
     for j in range(im.size[1]):
@@ -97,7 +111,12 @@ def main(img_path, dims=None):
             g = pix[1]
             b = pix[2]
             a = pix[3]
-            char = get_char_from_alpha(a)
+            if char_by_brightness:
+                char = get_char_from_brightness(r, g, b)
+                if a < 20:  # prevent weird artifacting from resize...
+                    char = ' '
+            else:
+                char = get_char_from_alpha(a)
 
             output += f"\033[38;2;{r};{g};{b}m{char}" * 2 + "\033[38;2;255;255;255m"
         output += "\n"  # line break at end of each row
@@ -116,22 +135,49 @@ if __name__ == "__main__":
     interpret args,
     call main
     """
+    cell_warning = 'note that a cell is roughly square, i.e. it is '\
+        '2 terminal characters wide and 1 terminal character tall.'
+
     argparser = argparse.ArgumentParser()
+
     argparser.add_argument('-d', '--debug', action='store_true',
-            required=False, default=False,
-            help='reduce output dims, may print helpful info')
+        required=False, default=False,
+        help='reduce output dims, print helpful info')
+
     # TODO handle bad indiv. values and combinations of these
     argparser.add_argument('-H', '--max-height', action='store', type=int,
-            required=False, help='WIP; restrict output to this many rows at most')
+        required=False, default=None,
+        help='Restrict output to this many rows at most; ' + cell_warning)
+
     argparser.add_argument('-W', '--max-width', action='store', type=int,
-            required=False, help='WIP; restrict output to this many columns at most')
+        required=False, default=None,
+        help='Restrict output to this many columns at most; ' + cell_warning)
+
+    argparser.add_argument('-b', '--char-by-brightness', action='store_true',
+        required=False, default=False, help='Use brightness (instead of '
+        'alpha) to determine character used to represent an input region in output')
+
+    argparser.add_argument('-c', '--char-offset', action='store', type=int,
+        required=False, default=0, help='remove <arg> chars from output options; '
+        f'sometimes block chars are ugly so you may want to use -c 4, for example. '
+        'Available chars are: {CHARS}')
+
     argparser.add_argument('imageFilePath')
     args = argparser.parse_args()
 
     DEBUG = args.debug
-    dims = None
-    if args.max_height is not None and args.max_width is not None:
-        dims = (args.max_height, args.max_width)
+    offset = args.char_offset
 
-    main(args.imageFilePath, dims)
+
+    CHARS = ['\u2588', '\u2593', '\u2592', '\u2591', '#', '@', '$', '+', '-', ' ']
+    NUM_CHARS = len(CHARS)
+    if offset >= NUM_CHARS - 1:
+        print(f'error: character offset argument must be < {NUM_CHARS-1} to get any '
+            f'visible output; see `{__file__} -h`.')
+        exit(1)
+    CHARS = CHARS[offset:]
+    INTERVALS = [255/len(CHARS)*i for i in range(len(CHARS)-1, -1, -1)]
+    if DEBUG:
+        print(f'working with these chars: {CHARS}')
+    main(args.imageFilePath, args.max_height, args.max_width, args.char_by_brightness)
 
