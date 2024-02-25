0ee4b4a524f84bdea36d1341c1852cf3cd746957
Amelia Miner <ameliamminer@gmail.com>
2024-02-25 13:41:39 -0800

    updated meofetch... works now... still curious as to why

--- meofetch.py
+++ meofetch.py
@@ -1,6 +1,6 @@
 import os
 
-# write output from asciifier into this file
+# write this file with image_printer_go_brrr.py
 txtpath = './img.txt'
 ascii_art = os.popen(f'cat {txtpath}').read()
 ascii_lines = ascii_art.split('\n')
@@ -11,8 +11,8 @@ sysinfo_lines = system_info.split('\n')
 for ascii_line, sysinfo_line in zip(ascii_lines, sysinfo_lines):
     print(ascii_line, sysinfo_line)
 
-# this resets the cursor
-print(os.popen('tput cnorm').read())
-# but something is still mega borked...
-# seems to be related to sysinfo_line, not ascii_line???
+# This is necessary due to something about the output
+# from neofetch... but why???
+print(os.popen('tput cnorm').read(), end='')
+print(os.popen('setterm --linewrap on').read(), end='')
 
