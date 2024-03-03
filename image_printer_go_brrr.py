6055d96b1288d35574caf7c97eea5b8e77e484de
Amelia Miner <ameliamminer@gmail.com>
2024-03-02 16:56:00 -0800

    cell class, removed some very silly bugs in new code

--- image_printer_go_brrr.py
+++ image_printer_go_brrr.py
@@ -1,18 +1,14 @@
 """
- Known bugs (have been tweaking, unsure if still present as of right now):
- * at large terminal sizes (very zoomed out) image takes on diamond-shaped
-   rgb artifacting?
- * at extreme terminal aspect ratios similar artifacting occurs as the image is
-   very slightly stretched along the long axis of the terminal window...
  TODO
- * This would be really cool if it could be consumed/reused by other programs
-   that produce a terminal interface of some kind e.x. curses, whatever -
-   it should provide an interface for its options other than command-line args.
- * Re-write with a class or two?
+ * Code is now reusable (except see below point), but significant refactoring is warranted
+ * eliminate use of debug arg!!
  * Would be cool, but maybe scale poorly, to do context-aware unicode blockchars
    ie have a left-half-block if pixel to left is opaque but right isn't
- * Probably still more cool arguments/switches I haven't thought up... and some I have:
+ * Probably still more cool arguments/switches I haven't thought up...
+   and some I have:
     - Add an option to output html/css instead of ansi escaped text file
+ * eliminate conflicting option interactions (-a and -f, for example)
+ * refine input sanitization
  * needs some kind of tests... oh boy
 """
 
@@ -23,161 +19,86 @@ from PIL import Image
 from sys import exit
 from os import path, get_terminal_size
 from time import sleep
-#from PIL import GifImagePlugin
-#GifImagePlugin.LOADING_STRATEGY = GifImagePlugin.LoadingStrategy.RGB_ALWAYS
 
 
-def resize_image_to_fit(im, max_height, max_width):
-    """
-    :param im: PIL.Image, thumbnail method will be used to resize iteratively
-        until the largest possible scaled-down dimensions are found
-        without losing correct aspect ratio
-    :param max_height: int, maximum number of character-pixels... vertical
-        need a better name for visual single-characters in output
-    :param max_width: int, maximum number of character-pixels, horizontal
-    :return: PIL.Image, scaled down to max size that fits provided dimensions
-    """
-    new_height, new_width = max_height, max_width
-    problem_dim = max(new_width, new_height)
-    im.thumbnail((problem_dim, problem_dim), RESIZE_METHOD)
-
-    while im.size[0] > max_width or im.size[1] > max_height:  # img > terminal
-        if DEBUG:
-            print(f'input image scaled to {im.size}')
-        problem_dim = max(new_width, new_height)
-        im.thumbnail((problem_dim, problem_dim), RESIZE_METHOD)
-        new_height -= 1
-        new_width -= 1
-
-    return im
+CHARS = ['\u2588', '\u2593', '\u2592', '\u2591',
+         '@', '#', '$', '+', '-', ' ']
 
 
-def get_char_from_alpha(a):
-    """
-    Takes in a transparency value and maps it to a character. The idea
-    is that the more transparent an image's pixel is, the less "foreground"
-    its representative terminal character should have. This is why this
-    program is unicode-only... TODO adapt based on runtime environment
-    :param a: int, min 0, max 255, alpha value of a pixel
-    :return: character fitting alpha value
-    """
-    for i, interval in enumerate(INTERVALS):
-        if a >= interval:
-            return CHARS[i]
-    return '🚩'  #UNICODE_ONLY
+class Cell():
+
+    def __init__(self, pixel, from_brightness=False,
+                 chars=None):
+        if chars is None:
+            self.chars = CHARS
+        else:
+            self.chars = chars
+        self.intervals = [255/len(self.chars)*i
+                          for i in range(len(self.chars)-1, -1, -1)]
+        self.pixel = pixel
+        self.from_brightness = from_brightness
+        self.char = self.get_char()
+        r = self.pixel[0]
+        g = self.pixel[1]
+        b = self.pixel[2]
+        self.color_escape = f"\033[38;2;{r};{g};{b}m" 
+        self.reset_escape = "\033[38;2;255;255;255m"
 
 
-def get_char_from_brightness(r, g, b):
-    """
-    Takes in a rgb values and maps their total brightness to a character.
-    Sometimes this is cuter than using alpha (in small outputs for example)
-    :param r: int, red value, max. 255
-    :param g: int, green value, max. 255
-    :param b: int, blue value, max. 255
-    :return: character fitting brightness value
-    """
-    brightness = r + g + b  # max. 765
-    for i, interval in enumerate(INTERVALS):
-        if brightness >= interval*3:
-            return CHARS[i]
-    return CHARS[-1]  # unsure of whether this is a good idea... may mask issues
-    return '🚩'  #UNICODE_ONLY
+    def __str__(self):
+        ret = str(self.color_escape + self.char) * 2\
+            + self.reset_escape  # TODO limit resets?
+        return ret
 
 
-def image_to_ascii(im):
-    output = []
-    for j in range(im.size[1]):
-        for i in range(im.size[0]):
-            pix = im.getpixel((i, j))
-            r = pix[0]
-            g = pix[1]
-            b = pix[2]
-            a = pix[3]
-            if CHAR_BY_BRIGHTNESS:
-                char = get_char_from_brightness(r, g, b)
-                if a < 20:  # prevent weird artifacting from resize...
-                    char = ' '
-            else:
-                char = get_char_from_alpha(a)
-
-            output += f"\033[38;2;{r};{g};{b}m{char}" * 2\
-                + "\033[38;2;255;255;255m"
-        output += "\n"  # line break at end of each row
-    return output
+    def get_char(self):
+        if self.from_brightness:
+            return self.get_char_from_brightness()
+        else:
+            return self.get_char_from_alpha()
 
 
-def print_animated(im, delay):
-    frames = []
-    for frame_index in range(im.n_frames):
-        im.seek(frame_index)
-        print(f'appending {im}')
-        frames.append(im)
-    ascii_frames = [image_to_ascii(frame) for frame in frames]
-    for a_frame in ascii_frames:
-        print(a_frame)
-        sleep(delay / 100000.0)
+    def get_char_from_brightness(self):
+        char = None
+        r = self.pixel[0]
+        g = self.pixel[1]
+        b = self.pixel[2]
+        brightness = r + g + b  # max. 765
+        for i, interval in enumerate(self.intervals):
+            if brightness >= interval*3:
+                char = self.chars[i]
+                break
+
+        if char is None:
+            char = '🚩'  #UNICODE_ONLY
+
+        return char
 
 
-def print_image(output):
-    for c in output:
-        print(c, end="")
+    def get_char_from_alpha(self):
+        #TODO rgba check
+        char = None
+        a = self.pixel[3]
+        for i, interval in enumerate(self.intervals):
+            if a >= interval:
+                char = self.chars[i]
+                break
 
+        if char is None:
+            char = '🚩'  #UNICODE_ONLY
+        elif a < 20:  # prevent weird artifacting from resize...
+            char = ' '
 
-def main(img_path, max_height=None, max_width=None):
-    """
-    get terminal dimensions,
-    generate output,
-    print output to terminal
-    :param img_path: str, path to image file
-    :param max_height: int, maximum number of output rows. 1 row == 1 char.
-        None defaults to terminal size.
-    :param max_width: int, maximum number of output columns. 1 col == 2 chars.
-        None defaults to terminal size.
-    """
-    if DEBUG:
-        print(f'Begin processing {img_path}')
-    # assume a terminal character is about twice as tall as it is wide
-    if max_height is None:
-        max_height = get_terminal_size().lines - 1  # -1 accounts for prompt
-    if max_width is None:
-        max_width = get_terminal_size().columns // 2
-    im = Image.open(img_path)  # TODO error handling af
-    if ANIMATE <= 0 and not CHAR_BY_BRIGHTNESS:
-        im = im.convert('RGBA')
-    if DEBUG:
-        print(f'Original dims h:{im.height}, w:{im.width}\n'
-              f'Maximum dims h:{max_height}, w:{max_width}')
-    im = resize_image_to_fit(im, max_height, max_width)
-    if DEBUG:
-        print(f'Resized dims h:{im.height}, w:{im.width}')
-
-    if ANIMATE == 0:
-        output = image_to_ascii(im)
-        print_image(output)
-        if args.save_to_file is not None:
-            if DEBUG:
-                print(f'Writing output to {args.save_to_file}')
-            with open(args.save_to_file, 'w') as wf:
-                for c in output:
-                    wf.write(c)
-    # TODO handle fake gifs?
-    elif args.save_to_file is None and img_path.endswith('.gif'):
-        print_animated(im, ANIMATE)
-    else:
-        print(f'Incompatible settings: '
-            '--save-to-file and --animate; see {__file__} -h.')
-        exit(1)
-
-
-    if DEBUG:
-        print(f'Finished processing {img_path}')
-    exit(0)
+        return char
 
 
 class ImageFilePrinter():
 
-    def __init__(self, image_path, max_height=None, max_width=None, resize_method=Image.LANCZOS,
-            char_by_brightness=False, save_to_file=None, animate=False, debug=False):
+    def __init__(self, image_path, max_height=None, max_width=None,
+            resize_method=Image.LANCZOS, char_by_brightness=False, 
+            chars=CHARS, save_to_file=None, animate=False):
+        self.chars = chars
+        self.intervals = [255/len(CHARS)*i for i in range(len(CHARS)-1, -1, -1)]
         self.image_path = image_path
         self.max_height = max_height
         self.max_width = max_width
@@ -185,14 +106,16 @@ class ImageFilePrinter():
         self.char_by_brightness = char_by_brightness
         self.save_to_file = save_to_file
         self.animate = animate
-        self.debug = debug
         self.image_object = None
         if not path.exists(self.image_path):
             print(f'unable to find file {image_path}')
             exit(1)
         else:
-            if self.debug and self.max_height is None or self.max_width is None:
-                print('Getting terminal dimensions')
+            if args.debug:  # TODO find a better way to do this
+                print(f'working with these chars: {self.chars} (len {len(self.chars)})')
+                print(f'working with these intervals: {self.intervals} (len {len(self.intervals)})')
+                if self.max_height is None or self.max_width is None:
+                    print('Getting terminal dimensions')
             if self.max_height is None:
                 self.max_height = get_terminal_size().lines - 1  # -1 accounts for prompt
             if self.max_width is None:
@@ -201,74 +124,71 @@ class ImageFilePrinter():
             self.image_object = Image.open(image_path)  # TODO error handling af
 
 
-    def print_image(self):
+    def print_ascii(self):
         if self.animate:
             self.print_animated()
         else:
-            self.dump_image_to_stdout()
+            image = self.prepare_for_dump(self.image_object)
+            if args.debug:
+                print(f'dumping {image} to stdout')
+            self.print_array(self.image_to_ascii_array(image))
 
 
-    def dump_image_to_stdout(self, image=None):
-        if image is None:
-            image = self.image_object
-        image = image.convert('RGBA')
-        if self.debug:
-            print(f'before: {image.size}')
-        image.thumbnail((self.max_width, self.max_height),
-                self.resize_method)
-        if self.debug:
-            print(f'target: {(self.max_width, self.max_height)}')
-            print(f'after: {image.size}')
-        if self.debug:
-            print(f'dumping {image} to stdout')
-            #return
-        for c in self.image_to_ascii_array(image):
+    def print_array(self, ascii_array):
+        for c in ascii_array:
             print(c, end="")
 
 
-    def print_animated(self):
-        im = self.image_object
-        frames = []
+    def prepare_for_dump(self, image):
+        image = image.convert('RGBA')
+        if args.debug:
+            print(f'before: {image.size}')
+        image.thumbnail((self.max_width, self.max_height),
+                self.resize_method)
+        if args.debug:
+            print(f'target: {(self.max_width, self.max_height)}')
+            print(f'after: {image.size}')
+        return image
 
+
+    def print_animated(self):
+        image = self.image_object
+        frames = []
+        print(f'Processing frames from {self.image_path}...')
         while True:
-            if self.debug:
+            if args.debug:
                 print()
-                print(f'Processing frame {im.tell()}')
-            self.dump_image_to_stdout(im)
-            sleep(self.animate / 1000.0)
+                print(f'Processing frame {image.tell()}')
+            frame = self.prepare_for_dump(image)
+            frames.append(self.image_to_ascii_array(frame))
             try:
-                im.seek(im.tell() + 1)
+                image.seek(image.tell() + 1)
             except EOFError:
-                if self.debug:
+                if args.debug:
                     print(f'No more frames in image')
-                if args.loop_infinitely:
-                    im.seek(0)
-                else:
-                    break
+                break
+        done = False
+        while not done:
+            for frame in frames:
+                if args.debug:
+                    print(f'dumping {image} to stdout')
+                self.print_array(frame)
+                sleep(self.animate / 1000.0)
+                if not args.loop_infinitely:
+                    done = True
 
 
     def image_to_ascii_array(self, image=None):
         if image is None:
             image = self.image_object
-        if self.debug:
+        if args.debug:
             print(f'converting {image} to ascii array')
         ret = []
         for j in range(image.size[1]):
             for i in range(image.size[0]):
                 pix = image.getpixel((i, j))
-                r = pix[0]
-                g = pix[1]
-                b = pix[2]
-                a = pix[3]
-                if CHAR_BY_BRIGHTNESS:
-                    char = get_char_from_brightness(r, g, b)
-                    if a < 20:  # prevent weird artifacting from resize...
-                        char = ' '
-                else:
-                    char = get_char_from_alpha(a)
-
-                ret += f"\033[38;2;{r};{g};{b}m{char}" * 2\
-                    + "\033[38;2;255;255;255m"  # TODO limit resets?
+                cell = Cell(pix, self.char_by_brightness, self.chars)
+                ret += str(cell)
             ret += "\n"  # line break at end of each row
         return ret
 
@@ -280,8 +200,6 @@ if __name__ == "__main__":
     call main
     """
     #UNICODE_ONLY
-    CHARS = ['\u2588', '\u2593', '\u2592', '\u2591',
-        '@', '#', '$', '+', '-', ' ']
     NUM_CHARS = len(CHARS)
     resize_options = {  # input param: (PIL param, readable name)
         'lz': (Image.LANCZOS, 'Lanczos'),
@@ -375,7 +293,6 @@ if __name__ == "__main__":
     argparser.add_argument('image_path')
     args = argparser.parse_args()
 
-    DEBUG = args.debug
     RESIZE_METHOD = None
     CHAR_BY_BRIGHTNESS = args.char_by_brightness
     ANIMATE = args.animate
@@ -401,7 +318,7 @@ if __name__ == "__main__":
         CHARS = CHARS[-1::-1]
     #if args.no_whitespace:
         #CHARS.remove(' ')
-    INTERVALS = [255/len(CHARS)*i for i in range(len(CHARS)-1, -1, -1)]
+    #INTERVALS = [255/len(CHARS)*i for i in range(len(CHARS)-1, -1, -1)]
     #if offset != 0:
         #INTERVALS = [i+INTERVALS[-1 - offset] for i in INTERVALS]
         #new_offset_addition = INTERVALS[-1*offset]
@@ -410,12 +327,12 @@ if __name__ == "__main__":
             #new_interval = interval+new_offset_addition
             #if new_interval <= 255:
                 #INTERVALS[i] = new_interval
-    if DEBUG:
-        print(f'working with these chars: {CHARS} (len {len(CHARS)})')
-        print(f'working with these intervals: {INTERVALS} (len {len(INTERVALS)})')
     #main(args.imageFilePath, args.max_height, args.max_width)
     resize_method = resize_options[args.resize_method][0]
-    image_printer = ImageFilePrinter(args.image_path, args.max_height, args.max_width,
-        resize_method, args.char_by_brightness, args.save_to_file, args.animate, args.debug)
-    image_printer.print_image()
+
+    image_printer = ImageFilePrinter(args.image_path,
+        args.max_height, args.max_width, resize_method,
+        args.char_by_brightness, CHARS,
+        args.save_to_file, args.animate)
+    image_printer.print_ascii()
 
