d167b89cbce4faf66fb6377952cfc0eaa99453b1
Amelia Miner <ameliamminer@gmail.com>
2024-03-02 20:53:19 -0800

    simplified meofetch example a little

--- meofetch.py
+++ meofetch.py
@@ -1,4 +1,6 @@
 #!/bin/python3
+# This only works on Linux (maybe Mac as well)
+# just a little example of how you might use the main script
 
 import os
 
@@ -11,14 +13,10 @@ system_info = os.popen('neofetch --off').read()
 sysinfo_lines = system_info.split('\n')
 
 for ascii_line, sysinfo_line in zip(ascii_lines, sysinfo_lines):
-    #if ':' in sysinfo_line and 'OS' not in sysinfo_line:
-        #head, line = sysinfo_line.split(':')
-        #sysinfo_line = head + ': <your output here>'
     print(ascii_line, sysinfo_line)
 
 # This is necessary due to something about the output
 # from neofetch... but why??? Something about cooked vs raw chars?
-#LINUX_ONLY
 print(os.popen('tput cnorm').read(), end='')
 print(os.popen('setterm --linewrap on').read(), end='')
 
